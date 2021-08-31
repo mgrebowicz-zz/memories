4f20e74ae537578fa1fffc21665a00fe1308ff73
Michael Grebowicz <maciejgrebowicz@gmail.com>
2021-08-31 14:36:15 -0700

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import { directive } from '@babel/types';
+import React from 'react';
+
+const App = () => {
+    return (
+        <div>
+            <h1>App</h1>
+        </div>
+    ); 
+}
+
+export default App;
