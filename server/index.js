f126f4b2e614f589842558e9ce86140ff92b349a
Michael Grebowicz <maciejgrebowicz@gmail.com>
2021-08-31 15:50:45 -0700

    setup server/index.js

--- server/index.js
+++ server/index.js
@@ -1,5 +1,26 @@
 import express from 'express';
-import bodyParser from 'body-parser';
+// import bodyParser from 'body-parser';
 import cors from 'cors';
 import mongoose from 'mongoose';
+
+const app = express();
+
+//bodyParser depricated
+// app.use(bodyParser.json({ limit: "30mb", extended: true }));
+// app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(express.urlencoded({ limit: '30mb', extended: true }));
+app.use(express.json({ limit: '30mb' }))
+
+app.use(cors());
+
+// https://www.mongodb.com/cloud/atlas
+// temporary connection password for testing
+
+const CONNECTION_URL = 'mongodb+srv://mgrebowicz:mgrebowicz123@cluster0.2ylkk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)) )
+    .catch((error) => console.log(error.message))
+
 
