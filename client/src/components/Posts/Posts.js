797b49947e918d453188f92393f1d9de92518c78
Michael Grebowicz <maciejgrebowicz@gmail.com>
2021-09-08 13:51:03 -0700

    code post card and add background

--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -1,5 +1,8 @@
+import { Grid, CircularProgress } from '@material-ui/core';
 import React from 'react';
 import { useSelector } from 'react-redux';
+
+
 import Post from './Post/Post';
 import useStyles from './styles';
 
@@ -10,11 +13,15 @@ const Posts = () => {
     console.log(posts);
     
     return (
-        <>
-            <h1>Posts</h1>
-            <Post />
-            <Post />
-        </>    
+        !posts.length ? <CircularProgress /> : (
+            <Grid className={classes.container} alignItems="stretch" spacing={3}>
+                {posts.map((post) => (
+                    <Grid key={post._id} item xs={12} sm={6}>
+                        <Post post={post}/>
+                    </Grid>
+                ))}
+            </Grid>
+        )
     );
 }
 
